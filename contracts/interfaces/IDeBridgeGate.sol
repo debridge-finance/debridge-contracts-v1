99949915ac5e371a3cf76f761382d67ca87b9328
artyukh <yaroslavartyukh@gmail.com>
2021-07-27 13:17:22 +0300

    Clean DeBridgeGate, add BPS

--- contracts/interfaces/IDeBridgeGate.sol
+++ contracts/interfaces/IDeBridgeGate.sol
@@ -11,7 +11,7 @@ interface IDeBridgeGate {
         uint256 maxAmount; // minimal amount to transfer
         uint256 collectedFees; // total collected fees that can be used to buy LINK
         uint256 balance; // total locked assets
-        uint256 minReserves; // minimal hot reserves
+        uint256 minReservesBPS; // minimal hot reserves in BPS
         mapping(uint256 => uint256) getChainFee; // whether the chain for the asset is supported
         bool exist;
     }
@@ -24,7 +24,7 @@ interface IDeBridgeGate {
     struct ChainSupportInfo {
         bool isSupported; // whether the chain for the asset is supported
         uint256 fixedNativeFee; // transfer fixed fee
-        uint256 transferFee; // transfer fee rate (in % of transferred amount)
+        uint256 transferFeeBPS; // transfer fee rate (in % of transferred amount)
     }
 
     /* ========== FUNCTIONS ========== */
