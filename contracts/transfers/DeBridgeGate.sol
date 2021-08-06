6396c46e0f2a012f713f2b33d81a45e60ec58502
artyukh <yaroslavartyukh@gmail.com>
2021-08-06 23:58:31 +0300

    Fix require in requestReserves

--- contracts/transfers/DeBridgeGate.sol
+++ contracts/transfers/DeBridgeGate.sol
@@ -687,7 +687,7 @@ contract DeBridgeGate is Initializable,
         bytes32 debridgeId = getDebridgeId(chainId, _tokenAddress);
         DebridgeInfo storage debridge = getDebridge[debridgeId];
         uint256 minReserves = (debridge.balance * debridge.minReservesBps) / BPS_DENOMINATOR;
-        require(minReserves + _amount > getBalance(debridge.tokenAddress), "requestReserves: not enough reserves");
+        require(minReserves + _amount < getBalance(debridge.tokenAddress), "not enough reserves");
         if (debridge.tokenAddress == address(0)) {
             payable(address(defiController)).transfer(_amount);
         } else {
