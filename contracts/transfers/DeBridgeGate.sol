219936acc925767f118fa8d2520e8420a4e76581
idealatom <idealatom@gmail.com>
2021-09-28 19:13:04 +0700

    add IERC20Permit.sol

--- contracts/transfers/DeBridgeGate.sol
+++ contracts/transfers/DeBridgeGate.sol
@@ -3,12 +3,12 @@ pragma solidity 0.8.7;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/draft-IERC20Permit.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
+import "../interfaces/IERC20Permit.sol";
 import "../interfaces/IDeBridgeToken.sol";
 import "../interfaces/IDeBridgeTokenDeployer.sol";
 import "../interfaces/ISignatureVerifier.sol";
