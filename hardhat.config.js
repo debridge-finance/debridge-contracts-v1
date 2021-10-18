4ea570f372e291fd1e5cbecbecf137f9beac16d5
artyukh <yaroslavartyukh@gmail.com>
2021-10-18 04:02:40 +0300

    update hardhat.config

--- hardhat.config.js
+++ hardhat.config.js
@@ -48,15 +48,6 @@ module.exports = {
         }
       },
       {
-        version: "0.8.7",
-        settings: {
-          optimizer: {
-            enabled: true,
-            runs: 200
-          }
-        }
-      },
-      {
         version: "0.6.11",
         settings: {
           optimizer: {
@@ -79,7 +70,7 @@ module.exports = {
   },
   networks: {
     hardhat: {
-      accounts:{mnemonic:process.env.MNEMONIC, count: 300},
+      accounts:{mnemonic:process.env.MNEMONIC},
       chainId: 1
     },
     development: {
