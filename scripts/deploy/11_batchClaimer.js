d43d0c32bce2830a4375487fac3ff7e3326b23f3
artyukh <yaroslavartyukh@gmail.com>
2021-11-11 18:03:47 +0300

    fix deploy batchClaimer

--- scripts/deploy/11_batchClaimer.js
+++ scripts/deploy/11_batchClaimer.js
@@ -1,4 +1,6 @@
 
+const { FLAGS, deployProxy, getLastDeployedProxy, waitTx } = require("../deploy-utils");
+
 module.exports = async function({getNamedAccounts, deployments, network}) {
   const { deployer } = await getNamedAccounts();
 
@@ -7,9 +9,13 @@ module.exports = async function({getNamedAccounts, deployments, network}) {
   console.log(`\tfrom DEPLOYER ${deployer}`);
   console.log('*'.repeat(80));
 
+  //No deployed proxy found for "DeBridgeGate"
   const deBridgeGate = await getLastDeployedProxy("DeBridgeGate", deployer);
-  await deployProxy("Claimer", deployer, deBridgeGate.address, true);
+  await deployProxy("Claimer", deployer,
+  //["0x68d936cb4723bdd38c488fd50514803f96789d2d"],
+  [deBridgeGate.address],
+  true);
 };
 
 module.exports.tags = ["11_batchClaimer"];
-module.exports.dependencies = ['08_DeBridgeGate'];
+module.exports.dependencies = [];// ['08_DeBridgeGate'];
