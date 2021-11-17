bd71843fa5285173206cf2890ee542000e6635e7
artyukh <yaroslavartyukh@gmail.com>
2021-11-18 01:31:33 +0300

    add simple feeProxy

---
+++ contracts/periphery/SimpleFeeProxy.sol
@@ -0,0 +1,127 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity 0.8.7;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
+import "../interfaces/IDeBridgeGate.sol";
+
+contract SimpleFeeProxy is Initializable, AccessControlUpgradeable, PausableUpgradeable {
+    using SafeERC20 for IERC20;
+
+    /* ========== STATE VARIABLES ========== */
+
+    IDeBridgeGate public debridgeGate;
+
+    address public treasuryAddress;
+
+    /* ========== ERRORS ========== */
+
+    error AdminBadRole();
+    error EmptyTreasuryAddress();
+    error EthTransferFailed();
+
+    /* ========== MODIFIERS ========== */
+
+    modifier onlyAdmin() {
+        if (!hasRole(DEFAULT_ADMIN_ROLE, msg.sender)) revert AdminBadRole();
+        _;
+    }
+
+    /* ========== CONSTRUCTOR  ========== */
+
+    function initialize() public initializer {
+        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+    }
+
+    /* ========== FUNCTIONS  ========== */
+
+    function pause() external onlyAdmin {
+        _pause();
+    }
+
+    function unpause() external onlyAdmin whenPaused {
+        _unpause();
+    }
+
+    function setDebridgeGate(IDeBridgeGate _debridgeGate) external onlyAdmin {
+        debridgeGate = _debridgeGate;
+    }
+
+    function setTreasury(address _treasuryAddress) external onlyAdmin {
+        treasuryAddress = _treasuryAddress;
+    }
+
+    /// @dev Transfer tokens to native chain and then create swap to deETH
+    /// and transfer reward to Ethereum network.
+    function withdrawFee(address _tokenAddress) external whenNotPaused {
+        (uint256 nativeChain, bytes memory nativeAddress) = debridgeGate.getNativeTokenInfo(
+            _tokenAddress
+        );
+        bytes32 debridgeId = getbDebridgeId(nativeChain, nativeAddress);
+
+        if (treasuryAddress == address(0)) revert EmptyTreasuryAddress();
+
+        debridgeGate.withdrawFee(debridgeId);
+        IERC20(_tokenAddress).safeTransfer(
+            treasuryAddress,
+            IERC20(_tokenAddress).balanceOf(address(this))
+        );
+    }
+
+    /// @dev Swap native tokens to deETH and then transfer reward to Ethereum network.
+    function withdrawNativeFee() external  whenNotPaused {
+        uint256 chainId = getChainId();
+        if (treasuryAddress == address(0)) revert EmptyTreasuryAddress();
+
+        debridgeGate.withdrawFee(getDebridgeId(chainId, address(0)));
+        uint256 amount = address(this).balance;
+
+         _safeTransferETH(treasuryAddress, amount);
+    }
+
+    // accept ETH
+    receive() external payable {}
+
+    /* ========== VIEW FUNCTIONS  ========== */
+
+    /// @dev Calculates asset identifier.
+    /// @param _chainId Current chain id.
+    /// @param _tokenAddress Address of the asset on the other chain.
+    function getbDebridgeId(uint256 _chainId, bytes memory _tokenAddress)
+        public
+        pure
+        returns (bytes32)
+    {
+        return keccak256(abi.encodePacked(_chainId, _tokenAddress));
+    }
+
+    function getDebridgeId(uint256 _chainId, address _tokenAddress) public pure returns (bytes32) {
+        return keccak256(abi.encodePacked(_chainId, _tokenAddress));
+    }
+
+    /* ========== PRIVATE FUNCTIONS  ========== */
+
+    function getChainId() public view virtual returns (uint256 cid) {
+        assembly {
+            cid := chainid()
+        }
+    }
+
+    /*
+    * @dev transfer ETH to an address, revert if it fails.
+    * @param to recipient of the transfer
+    * @param value the amount to send
+    */
+    function _safeTransferETH(address to, uint256 value) internal {
+        (bool success, ) = to.call{value: value}(new bytes(0));
+        if (!success) revert EthTransferFailed();
+    }
+
+    // ============ Version Control ============
+    function version() external pure returns (uint256) {
+        return 101; // 1.0.1
+    }
+}
--- scripts/deploy/06_FeeProxy.js
+++ scripts/deploy/06_FeeProxy.js
@@ -6,11 +6,15 @@ module.exports = async function({getNamedAccounts, deployments, network}) {
   const deployInitParams = debridgeInitParams[network.name];
   if (!deployInitParams) return;
 
-  const weth = deployInitParams.external.WETH || (await deployments.get("MockWeth")).address;
-  const uniswapFactory = deployInitParams.external.UniswapFactory || (await deployments.get("UniswapV2Factory")).address;
+  // const weth = deployInitParams.external.WETH || (await deployments.get("MockWeth")).address;
+  // const uniswapFactory = deployInitParams.external.UniswapFactory || (await deployments.get("UniswapV2Factory")).address;
 
-  await deployProxy("FeeProxy", deployer, [uniswapFactory, weth], true);
+  // await deployProxy("FeeProxy", deployer, [uniswapFactory, weth], true);
 
+  await deployProxy("SimpleFeeProxy", deployer, [], true);
+
+
+  //We deployed simple proxy with function only to withdraw fee
   //TODO: FeeProxy setTreasury for each chains
   //TODO: FeeProxy setDeEthToken
   //TODO: FeeProxy setFeeProxyAddress for each chains
