b0a9715dd18f03124b4ff7391c6514fbb112f77c
vsevolod <5249361+Vsevo1od@users.noreply.github.com>
2021-11-20 17:34:01 +0200

    Add tests for SimpleFeeProxy.withdrawNativeFee

--- test/SimpleFeeProxy.test.ts
+++ test/SimpleFeeProxy.test.ts
@@ -19,10 +19,11 @@ let deployer: SignerWithAddress;
 let treasury: SignerWithAddress;
 let user: SignerWithAddress;
 let native: SignerWithAddress;
+let donor: SignerWithAddress;
 let CHAIN_ID: number;
 
 before(async () => {
-    [deployer, treasury, user, native] = await ethers.getSigners();
+    [deployer, treasury, user, native, donor] = await ethers.getSigners();
     CHAIN_ID =  (await ethers.provider.getNetwork()).chainId;
 });
 
@@ -40,11 +41,11 @@ it('grants DEFAULT_ADMIN_ROLE only to a deployer', async () => {
     expect(isUserAdmin).to.be.false;
 })
 
-describe('withdrawFee', () => {
+describe('withdraw', () => {
     let deBridgeGateMock: DeBridgeGateMock;
     let erc20Mock: ERC20Mock;
     let NATIVE_ADDRESS: Bytes;
-    
+
     before(() => {
         NATIVE_ADDRESS = arrayify(native.address);
     })
@@ -63,39 +64,86 @@ describe('withdrawFee', () => {
         await simpleFeeProxy.setDebridgeGate(deBridgeGateMock.address);
     })
 
-    it('reverts on 0 treasury address', async () => {
-        await expect(
-            simpleFeeProxy.withdrawFee(erc20Mock.address)
-        ).not.to.be.revertedWith("EmptyTreasuryAddress()");
-
-        await simpleFeeProxy.setTreasury(ethers.constants.AddressZero);
-        await expect(
-            simpleFeeProxy.withdrawFee(erc20Mock.address)
-        ).to.be.revertedWith("EmptyTreasuryAddress()");
+    describe('fee', () => {
+        it('reverts on 0 treasury address', async () => {
+            await expect(
+                simpleFeeProxy.withdrawFee(erc20Mock.address)
+            ).not.to.be.revertedWith("EmptyTreasuryAddress()");
+    
+            await simpleFeeProxy.setTreasury(ethers.constants.AddressZero);
+            await expect(
+                simpleFeeProxy.withdrawFee(erc20Mock.address)
+            ).to.be.revertedWith("EmptyTreasuryAddress()");
+        })
+    
+        it('calls withdrawFee with correct id', async () => {
+            const correctId = ethers.utils.solidityKeccak256(
+                ['uint256', 'bytes'], [CHAIN_ID, NATIVE_ADDRESS]
+            );
+    
+            await expectStubFunctionIsCalledWith(
+                deBridgeGateMock.mock.withdrawFee,
+                [correctId],
+                async () => simpleFeeProxy.withdrawFee(erc20Mock.address),
+            )        
+        })
+    
+        it('transfers correct amount to treasury', async () => {
+            const BALANCE_OF_SIMPLE_FEE_PROXY = 1234567890;
+    
+            erc20Mock.mock.balanceOf.withArgs(simpleFeeProxy.address)
+                .returns(BALANCE_OF_SIMPLE_FEE_PROXY);
+    
+            await expectStubFunctionIsCalledWith(
+                erc20Mock.mock.transfer,
+                [treasury.address, BALANCE_OF_SIMPLE_FEE_PROXY],
+                async () => simpleFeeProxy.withdrawFee(erc20Mock.address)
+            )
+        })
     })
 
-    it('calls withdrawFee with correct id', async () => {
-        const correctId = ethers.utils.solidityKeccak256(
-            ['uint256', 'bytes'], [CHAIN_ID, NATIVE_ADDRESS]
-        );
+    describe('nativeFee', () => {
+        it('reverts on 0 treasury address', async () => {
+            await expect(
+                simpleFeeProxy.withdrawNativeFee()
+            ).not.to.be.revertedWith("EmptyTreasuryAddress()");
+    
+            await simpleFeeProxy.setTreasury(ethers.constants.AddressZero);
+            await expect(
+                simpleFeeProxy.withdrawNativeFee()
+            ).to.be.revertedWith("EmptyTreasuryAddress()");
+        })
+    
+        it('calls withdrawFee with correct id', async () => {
+            const correctId = ethers.utils.solidityKeccak256(
+                ['uint256', 'bytes'], [CHAIN_ID, ethers.constants.AddressZero]
+            );
+    
+            await expectStubFunctionIsCalledWith(
+                deBridgeGateMock.mock.withdrawFee,
+                [correctId],
+                async () => simpleFeeProxy.withdrawNativeFee(),
+            )        
+        })
+    
+        it('transfers correct amount to treasury', async () => {
+            const BALANCE_OF_SIMPLE_FEE_PROXY = 1234567890;
+            
+            await donor.sendTransaction({
+                to: simpleFeeProxy.address, value: BALANCE_OF_SIMPLE_FEE_PROXY
+            })
 
-        await expectStubFunctionIsCalledWith(
-            deBridgeGateMock.mock.withdrawFee,
-            [correctId],
-            async () => simpleFeeProxy.withdrawFee(erc20Mock.address),
-        )        
-    })
+            expect(await ethers.provider.getBalance(simpleFeeProxy.address))
+                .to.equal(BALANCE_OF_SIMPLE_FEE_PROXY);
+            const treasuryBalance = await ethers.provider.getBalance(treasury.address);
 
-    it('transfers correct amount to treasury', async () => {
-        const BALANCE_OF_SIMPLE_FEE_PROXY = 1234567890;
+            await simpleFeeProxy.withdrawNativeFee();
 
-        erc20Mock.mock.balanceOf.withArgs(simpleFeeProxy.address)
-            .returns(BALANCE_OF_SIMPLE_FEE_PROXY);
-
-        await expectStubFunctionIsCalledWith(
-            erc20Mock.mock.transfer,
-            [treasury.address, BALANCE_OF_SIMPLE_FEE_PROXY],
-            async () => simpleFeeProxy.withdrawFee(erc20Mock.address)
-        )
+            expect(await ethers.provider.getBalance(simpleFeeProxy.address))
+                .to.equal(0);
+            expect(await ethers.provider.getBalance(treasury.address))
+                .to.equal(treasuryBalance.add(BALANCE_OF_SIMPLE_FEE_PROXY));
+        })
     })
 })
+
