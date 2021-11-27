1da864042e17f702f353f6d368580da59d40a9cc
artyukh <yaroslavartyukh@gmail.com>
2021-11-27 23:55:51 +0300

    add ControlWalletProxy

---
+++ contracts/periphery/ControlWalletProxy.sol
@@ -0,0 +1,121 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity 0.8.7;
+
+import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
+import "./CallProxy.sol";
+
+contract ControlWalletProxy is Initializable, AccessControlUpgradeable, PausableUpgradeable {
+    using SafeERC20 for IERC20;
+
+    /* ========== STATE VARIABLES ========== */
+
+    // chainId => list of addresses that can control this contract
+    mapping(uint256 => mapping(bytes => bool)) public controlParams;
+    CallProxy callProxy;
+
+    /* ========== ERRORS ========== */
+
+    error AdminBadRole();
+    error NativeSenderBadRole(bytes nativeSender, uint256 chainIdFrom);
+    error CallProxyBadRole();
+
+    error ExternalCallFailed();
+
+    /* ========== EVENTS ========== */
+
+    event CallProxyUpdated(address callProxy); // emited when the new call proxy set
+
+    event CrontrollingAddressUpdated(bytes nativeSender, uint256 chainIdFrom, bool enabled);
+
+    /* ========== MODIFIERS ========== */
+
+    modifier onlyAdmin() {
+        if (!hasRole(DEFAULT_ADMIN_ROLE, msg.sender)) revert AdminBadRole();
+        _;
+    }
+
+    modifier onlyCallProxy() {
+        if (address(callProxy) != msg.sender) revert CallProxyBadRole();
+        _;
+    }
+
+    /* ========== CONSTRUCTOR  ========== */
+
+    function initialize(CallProxy _callProxy) public initializer {
+        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        callProxy = _callProxy;
+    }
+
+    function call(
+        address destination,
+        bytes memory data
+    ) external payable onlyCallProxy whenNotPaused returns (bool _result) {
+        uint256 amount = address(this).balance;
+        bytes memory nativeSender = callProxy.submissionNativeSender();
+        uint256 chainIdFrom = callProxy.submissionChainIdFrom();
+
+        if(!controlParams[chainIdFrom][nativeSender]) revert NativeSenderBadRole(nativeSender, chainIdFrom);
+
+        _result = externalCall(
+            destination,
+            amount,
+            data
+        );
+
+        if (!_result) {
+            revert ExternalCallFailed();
+        }
+    }
+
+
+    //gnosis
+    //https://github.com/gnosis/MultiSigWallet/blob/ca981359cf5acd6a1f9db18e44777e45027df5e0/contracts/MultiSigWallet.sol#L244-L261
+
+    function externalCall(
+        address destination,
+        uint256 value,
+        bytes memory data
+    ) internal returns (bool result) {
+        uint256 dataLength = data.length;
+        assembly {
+            let x := mload(0x40) // "Allocate" memory for output (0x40 is where "free memory" pointer is stored by convention)
+            let d := add(data, 32) // First 32 bytes are the padded length of data, so exclude that
+            result := call(
+                sub(gas(), 34710), // 34710 is the value that solidity is currently emitting
+                // It includes callGas (700) + callVeryLow (3, to pay for SUB) + callValueTransferGas (9000) +
+                // callNewAccountGas (25000, in case the destination address does not exist and needs creating)
+                destination,
+                value,
+                d,
+                dataLength, // Size of the input (in bytes) - this is what fixes the padding problem
+                x,
+                0 // Output is ignored, therefore the output size is zero
+            )
+        }
+    }
+
+    /// @dev Set proxy address.
+    /// @param _callProxy Address of the proxy that executes external calls.
+    function setCallProxy(CallProxy _callProxy) external onlyAdmin {
+        callProxy = _callProxy;
+        emit CallProxyUpdated(address(_callProxy));
+    }
+
+    function setControlingAddress(bytes memory _nativeSender, uint256 _chainIdFrom, bool enabled) external onlyAdmin {
+        controlParams[_chainIdFrom][_nativeSender] = enabled;
+        emit CrontrollingAddressUpdated(_nativeSender, _chainIdFrom, enabled);
+    }
+
+    function getControlingAddress(bytes memory _nativeSender, uint256 _chainIdFrom) external view returns (bool) {
+        return controlParams[_chainIdFrom][_nativeSender];
+    }
+
+    // ============ Version Control ============
+    function version() external pure returns (uint256) {
+        return 101; // 1.0.1
+    }
+}
