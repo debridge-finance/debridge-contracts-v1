8d23f3f19359d59c13ee598038edb9cad589618e
artyukh <yaroslavartyukh@gmail.com>
2021-11-30 23:41:46 +0300

    logs. upgraded testnet contracts

--- scripts/deploy/16_2_upgrade_testnet_29.11.js
+++ scripts/deploy/16_2_upgrade_testnet_29.11.js
@@ -14,9 +14,15 @@ module.exports = async function({getNamedAccounts, deployments, network}) {
     throw Error('Undefined treasury address!');
   }
 
+  if (deployInitParams.deploy.wethGate && !deployInitParams.external.WETH) {
+    throw Error('Undefined WETH address!');
+  }
+
+
   console.log('*'.repeat(80));
   console.log('\t16_upgrade_testnet_29.11');
   console.log('*'.repeat(80));
+
 
   const DEBRIDGE_GATE_ADDRESS = '0x68D936Cb4723BdD38C488FD50514803f96789d2D';
   const CALL_PROXY_ADDRESS = '0xEF3B092e84a2Dbdbaf507DeCF388f7f02eb43669';
@@ -44,48 +50,16 @@ module.exports = async function({getNamedAccounts, deployments, network}) {
     await waitTx(tx);
   }
 
-
-  // 2. Upgrade CallProxy
-  // Changes:
-  // * removed variation variable
-  // * added submissionChainIdFrom, submissionNativeSender variables
-  console.log('2. Upgrade CallProxy')
-  const { contract: callProxy } = await upgradeProxy(
-    "CallProxy",
-    CALL_PROXY_ADDRESS,
-    deployer);
-
-  // 2.1 Set CallProxy in DebridgeGate
-  console.log(`2.1 Set CallProxy in DebridgeGate - Calling DeBridgeGate.setCallProxy(${callProxy.address})`)
-  tx = await deBridgeGateInstance.setCallProxy(callProxy.address);
-  await waitTx(tx);
-
-  // 3. Deploy WethGate
-  if (deployInitParams.deploy.wethGate) {
-    console.log('3. Deploy WethGate');
-    const WETH = deployInitParams.external.WETH || (await deployments.get("MockWeth")).address;
-    const wethGate = await deploy("WethGate", {
-      from: deployer,
-      args: [WETH],
-      log: true,
-    });
-
-    // 3.1 Set WethGate in DebridgeGate
-    console.log(`3.1 Set WethGate in DebridgeGate - Calling DeBridgeGate.setWethGate(${wethGate.address})`);
-    tx = await deBridgeGateInstance.setWethGate(wethGate.address);
-    await waitTx(tx);
-  }
-
-
-  // 4. Upgrade DebrideGate
-  console.log('4. Upgrade DebrideGate');
+  // 2. Upgrade DebrideGate
+  console.log('2. Upgrade DebrideGate');
   const { contract: upgradedDeBridgeGateInstance } = await upgradeProxy(
     "DeBridgeGate",
     DEBRIDGE_GATE_ADDRESS,
     deployer);
 
-  // 4.1 Update getChainFromConfig mapping
-  console.log(`4.1 Update getChainFromConfig mapping - Calling DeBridgeGate.updateChainSupport`);
+
+  // 2.1 Update getChainFromConfig mapping
+  console.log(`2.1 Update getChainFromConfig mapping - Calling DeBridgeGate.updateChainSupport`);
   console.log('chainIds: ', deployInitParams.supportedChains);
   console.log('chainSupportInfo: ', deployInitParams.chainSupportInfo);
   tx = await upgradedDeBridgeGateInstance.updateChainSupport(
@@ -95,6 +69,40 @@ module.exports = async function({getNamedAccounts, deployments, network}) {
   );
   await waitTx(tx);
 
+  // 3. Upgrade CallProxy
+  // Changes:
+  // * removed variation variable
+  // * added submissionChainIdFrom, submissionNativeSender variables
+  console.log('3. Upgrade CallProxy')
+  const { contract: callProxy } = await upgradeProxy(
+    "CallProxy",
+    CALL_PROXY_ADDRESS,
+    deployer);
+
+  // 3.1 Set CallProxy in DebridgeGate
+  console.log(`3.1 Set CallProxy in DebridgeGate - Calling DeBridgeGate.setCallProxy(${CALL_PROXY_ADDRESS})`)
+  tx = await upgradedDeBridgeGateInstance.setCallProxy(CALL_PROXY_ADDRESS);
+   // await deBridgeGateInstance.setCallProxy(CALL_PROXY_ADDRESS);
+  await waitTx(tx);
+
+
+  // 4. Deploy WethGate
+  if (deployInitParams.deploy.wethGate) {
+    console.log('4. Deploy WethGate');
+    const WETH = deployInitParams.external.WETH; // || (await deployments.get("MockWeth")).address;
+    const wethGate = await deploy("WethGate", {
+      from: deployer,
+      args: [WETH],
+      log: true,
+    });
+
+    // 4.1 Set WethGate in DebridgeGate
+    console.log(`4.1 Set WethGate in DebridgeGate - Calling DeBridgeGate.setWethGate(${wethGate.address})`);
+    tx = await upgradedDeBridgeGateInstance.setWethGate(wethGate.address);
+     // deBridgeGateInstance.setWethGate(wethGate.address);
+    await waitTx(tx);
+  }
+
 
   // 5. DeBridgeTokenDeployer
   // Don't upgrade it because WETH deTokens already were deployed.
@@ -102,4 +110,4 @@ module.exports = async function({getNamedAccounts, deployments, network}) {
   // * added overridedTokens mapping
 };
 
-module.exports.tags = ["16_upgrade_testnet_29.11"];
+module.exports.tags = ["16_2_upgrade_testnet_29.11"];
