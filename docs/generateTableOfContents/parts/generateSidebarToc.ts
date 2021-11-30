becf0de73cd7b56a1a35d3a5c6ebd30a29e59620
vsevolod <5249361+Vsevo1od@users.noreply.github.com>
2021-11-30 16:38:18 +0300

    Remove flattening of sub dirs in SUMMARY.md

--- docs/generateTableOfContents/parts/generateSidebarToc.ts
+++ docs/generateTableOfContents/parts/generateSidebarToc.ts
@@ -1,5 +1,5 @@
 import {upperFirst} from "lodash";
-import {DirEntry, FileEntry, ListOfFiles} from "./types/ListOfFiles";
+import {DirEntry, ListOfFiles} from "./types/ListOfFiles";
 import {isDir, isFile, notReadme} from "./filters";
 import {toLink} from "./toLink";
 import {CONTRACTS_PATH_IN_DOCS, README_NAME} from "./constants";
@@ -11,44 +11,34 @@ export default function generateSidebarToc(listOfFiles: ListOfFiles): string {
     const fileEntries = entries.filter(isFile).filter(notReadme);
     const dirEntries = entries.filter(isDir);
 
-    const dirLinks = dirEntries.map(generateDirLinksWithSubLinks);
+    const dirLinks = dirEntries.map(entry => generateDirLinksWithSubLinks(entry));
     const fileLinks = fileEntries.map(([name, path]) => toLink(name, CONTRACTS_PATH_IN_DOCS + path));
 
     return title + [...dirLinks, ...fileLinks].join('\n');
 }
 
-function generateDirLinksWithSubLinks([name, content]: DirEntry): string {
-    const dirLink = toLink(upperFirst(name), `${CONTRACTS_PATH_IN_DOCS}${name}/${README_NAME}`);
+function generateDirLinksWithSubLinks(
+    [name, content]: DirEntry,
+    level = 1,
+    path = CONTRACTS_PATH_IN_DOCS
+): string {
+    const indent = (str: string): string => '  '.repeat(level) + str;
+
+    const dirLink = toLink(upperFirst(name), `${path}${name}/${README_NAME}`);
+    const dirLinkIndented = '  '.repeat(level - 1) + dirLink;
 
     const entries = Object.entries(content);
     const dirEntries = entries.filter(isDir);
     const fileEntries = entries.filter(isFile).filter(notReadme);
 
-    const dirsContentFlat: FileEntry[] = dirEntries
-        .map(([, content]) => content)
-        .map(flattenDirContent)
-        .flat()
-    ;
+    const subDirLinks = dirEntries.map(entry =>
+        generateDirLinksWithSubLinks(entry, level + 1, `${path}${name}/`)
+    ).join('\n');
 
-    const indent = (str: string) => `  ${str}`;
-    const subLinks = [...fileEntries, ...dirsContentFlat]
+    const subLinks = fileEntries
         .map(([fileName, filePath]) => toLink(fileName, CONTRACTS_PATH_IN_DOCS + filePath))
         .map(indent)
         .join('\n');
 
-    return `${dirLink}\n${subLinks}`;
-}
-
-function flattenDirContent(listOfFiles: ListOfFiles): FileEntry[] {
-    const entries = Object.entries(listOfFiles);
-    const dirEntries = entries.filter(isDir);
-    const fileEntries = entries.filter(isFile).filter(notReadme);
-
-    const dirContent: FileEntry[] = dirEntries
-        .map(([, content]) => content)
-        .map(flattenDirContent)
-        .flat()
-    ;
-
-    return [...dirContent, ...fileEntries];
+    return `${dirLinkIndented}\n${subLinks}\n${subDirLinks}`;
 }
