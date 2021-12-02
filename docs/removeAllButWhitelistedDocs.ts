176e2b937e34ffb233d9379970790eb2a010915d
vsevolod <5249361+Vsevo1od@users.noreply.github.com>
2021-12-02 15:05:33 +0300

    Refactor removeAllButWhitelistedDocs.ts

--- docs/removeAllButWhitelistedDocs.ts
+++ docs/removeAllButWhitelistedDocs.ts
@@ -3,15 +3,31 @@ import {CONTRACT_DOCS_PATH} from "./generateTableOfContents/parts/constants";
 import * as fs from "fs";
 import {sync as deleteEmptyDirsIn} from "delete-empty";
 import path from "path";
+import assert from "assert";
 
-const whitelistedDocs = [
-    'transfers/DeBridgeGate.md',
-    'transfers/DeBridgeTokenDeployer.md',
-    'transfers/SignatureVerifier.md',
-    'periphery/CallProxy.md',
+const whitelistedContracts = [
+    'transfers/DeBridgeGate.sol',
+    'transfers/DeBridgeTokenDeployer.sol',
+    'transfers/SignatureVerifier.sol',
+    'periphery/CallProxy.sol',
 ];
 
-const exceptions = whitelistedDocs.map(path => `!${CONTRACT_DOCS_PATH}${path}`);
-const filesToRemove = globby.sync([`${CONTRACT_DOCS_PATH}**/*.md`, ...exceptions]);
+const toContractPathFromProjectRoot = (path: string) => `contracts/${path}`;
+const assertExists = (path: string) => assert(fs.existsSync(path));
+whitelistedContracts
+    .map(toContractPathFromProjectRoot)
+    .forEach(assertExists)
+;
+
+const changeExtensionToMd = (contractPath: string) => contractPath.replace(/.sol$/, '.md');
+const toPathFromProjectRoot = (path: string) => `${CONTRACT_DOCS_PATH}${path}`;
+const whitelistedDocsAsExceptions = whitelistedContracts
+    .map(changeExtensionToMd)
+    .map(toPathFromProjectRoot)
+    .map(path => `!${path}`)
+;
+
+const filesToRemove = globby.sync([`${CONTRACT_DOCS_PATH}**/*.md`, ...whitelistedDocsAsExceptions]);
 filesToRemove.forEach(path => fs.unlinkSync(path));
+
 deleteEmptyDirsIn(path.resolve(CONTRACT_DOCS_PATH));
