e97aa6ff2ff7df1efef84572a171c4cc7f77d28c
vsevolod <5249361+Vsevo1od@users.noreply.github.com>
2021-12-07 21:11:27 +0300

    Add documented contracts to whitelist

--- scripts/docsGeneration/whitelistedContracts.ts
+++ scripts/docsGeneration/whitelistedContracts.ts
@@ -1,35 +1,47 @@
-// TODO merge when NatSpec for all the docs in Readme are ready
 import assert from "assert";
 import fs from "fs";
 
 const contractsWhitelistedForDocs = [
     'transfers/DeBridgeGate.sol',
     'interfaces/IDeBridgeGate.sol',
+
     'transfers/DeBridgeTokenDeployer.sol',
     'interfaces/IDeBridgeTokenDeployer.sol',
+
     'transfers/SignatureVerifier.sol',
     'interfaces/ISignatureVerifier.sol',
+
     'periphery/CallProxy.sol',
     'interfaces/ICallProxy.sol',
-    'libraries/Flags.sol'
-];
 
-const contractsWhitelistedForReadme = [
+    'libraries/Flags.sol',
+
     'transfers/AggregatorBase.sol',
-    'transfers/DeBridgeGate.sol',
-    'transfers/DeBridgeTokenDeployer.sol',
-    'transfers/SignatureVerifier.sol',
+    'interfaces/IAggregatorBase.sol',
+
     'transfers/WethGate.sol',
-    'periphery/CallProxy.sol',
+    'interfaces/IWethGate.sol',
+
     'periphery/DeBridgeToken.sol',
+    'interfaces/IDeBridgeToken.sol',
+
     'periphery/DeBridgeTokenProxy.sol',
+
     'periphery/SimpleFeeProxy.sol',
 ];
+
+assert(
+    (new Set(contractsWhitelistedForDocs)).size === contractsWhitelistedForDocs.length,
+    'contractsWhitelistedForDocs has duplicates'
+);
+
+const notInterfaceOrLibrary = (path: string) => !path.startsWith('libraries/') && !path.startsWith('interfaces/');
+const contractsWhitelistedForReadme = contractsWhitelistedForDocs.filter(notInterfaceOrLibrary);
 
 const toContractPathFromProjectRoot = (path: string) => `contracts/${path}`;
 const assertExists = (path: string) => assert(fs.existsSync(path), `${path} does not exist.`);
 
-[...contractsWhitelistedForDocs, ...contractsWhitelistedForReadme]
+contractsWhitelistedForDocs
     .map(toContractPathFromProjectRoot)
     .forEach(assertExists)
 
