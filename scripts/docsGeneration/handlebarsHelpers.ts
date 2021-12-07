39abde9594d4c2c08d7b4a8583e7a921e5615b9a
vsevolod <5249361+Vsevo1od@users.noreply.github.com>
2021-12-07 18:02:08 +0300

    Add parent dir for contracts description in readme

--- scripts/docsGeneration/handlebarsHelpers.ts
+++ scripts/docsGeneration/handlebarsHelpers.ts
@@ -1,4 +1,5 @@
 import {contractsWhitelistedForReadme} from "./whitelistedContracts";
+import {upperFirst} from "lodash";
 
 function getName(path: string): string {
     const nameWithExtension = path.split('/').pop();
@@ -11,4 +12,19 @@ const isWhitelisted = (name: string): boolean => {
     return contractNamesWhitelistedForReadme.includes(name);
 };
 
-export {isWhitelisted};
+const getPathWithoutFileNameUpperFirst = (path: string): string => {
+    const pathWithoutFileName = path.split('/').slice(0, -1).join('/');
+    return upperFirst(pathWithoutFileName);
+}
+
+const seenPaths: string[] = [];
+const isPathWithoutFileNameNeverSeen = (path: string): boolean => {
+    const pathWithoutFileName = getPathWithoutFileNameUpperFirst(path);
+    const isPathNeverSeen = !seenPaths.includes(pathWithoutFileName);
+    if (isPathNeverSeen){
+        seenPaths.push(pathWithoutFileName);
+    }
+    return isPathNeverSeen;
+}
+
+export {isWhitelisted, isPathWithoutFileNameNeverSeen, getPathWithoutFileNameUpperFirst};
